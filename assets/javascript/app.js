1669533000ffb4cea67db6b4d7e4c4154b1493c3
gadcad98 <37885704+gadcad98@users.noreply.github.com>
2018-06-04 21:39:10 -0700

    added images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,29 +5,51 @@
 // Create "radio" buttons for the user to set their answer to
 // Create a submit button
     // if all answers are input before the time's up the submit button counts correct/incorrect answers and displays them for the user
-// Create a clear or reset button
+// Create a clear or reset button that starts a new round
 
-var count = 10;
-        function timer(){
+
+
+
+
+// var button = document.getElementsByClassName("btn btn-warning");
+// var questions = document.getElementsByClassName("questions");
+
+
+
+var counter = 0;
+
+
+
+// $("#timer"). function timer(counter) {
+    
+// }
+    
+        button.onclick = function(){
+
                    
-            if(count > 0){
+            if(counter >= 0){
                 setTimeout(function(){
-                $("#timer").text(count)               
+                $("#timer").text(counter)               
 
                 timer();
 
 
             }, 1000)
 
-             count--;
+             counter--;
             }
             else{
-                $("#timer").text(count);
+                $("#timer").text(counter);
                 alert("Game over, man!!!");
             }
            
-        }
+        };
 
         timer();
 
+
+
+
+// Ladies and Gentlemen, this is... just horrible. Words can't express what we're all feeling at this very moment. A great champion has fallen. We'll be back right after these important messages.
+// You have... death around you.
     
